f29b3ee5a59289300901944345289f43880785f7
nhnhat <nhnhat@gmail.com>
2022-07-14 07:20:31 +0700

    Update base_kernel_game.erl

--- src/base_kernel_game.erl
+++ src/base_kernel_game.erl
@@ -11,6 +11,7 @@
 %% API
 -export([
   initRoom/1,
+  roomReady/1,
   joinRoom/2,
   leaveRoom/2,
   getPlayers/1,
@@ -25,34 +26,20 @@
   players_in_room/1
 ]).
 
-initRoom(RoomId) ->
-  base_lib_room:initRoom(RoomId).
+initRoom(RoomId) -> base_lib_room:initRoom(RoomId).
 
-joinRoom(RoomId,PlayerId)->
-  base_lib_room:joinRoom(RoomId,PlayerId).
-
-leaveRoom(RoomId,PlayerId)->
-  base_lib_room:leaveRoom(RoomId,PlayerId).
-
-getPlayersCount(RoomId)->
-  base_lib_room:getPlayersCount(RoomId).
-
-getPlayers(RoomId)->
-  base_lib_room:getPlayers(RoomId).
+roomReady(RoomId) -> base_lib_room:roomReady(RoomId).
+joinRoom(RoomId,PlayerId)-> base_lib_room:joinRoom(RoomId,PlayerId).
+leaveRoom(RoomId,PlayerId)-> base_lib_room:leaveRoom(RoomId,PlayerId).
+getPlayersCount(RoomId)-> base_lib_room:getPlayersCount(RoomId).
+getPlayers(RoomId)-> base_lib_room:getPlayers(RoomId).
 
 %% ------------------------------------------------------------------
 %% Apply Functions
 %% ------------------------------------------------------------------
-join_room(State,RoleId) ->
-  base_lib_room:join_room(State,RoleId).
-
-leave_room(State,RoleId) ->
-  base_lib_room:leave_room(State,RoleId).
-
-players_in_room(State) ->
-  base_lib_room:players_in_room(State).
-
-players_count(State) ->
-  base_lib_room:players_count(State).
+join_room(State,RoleId) -> base_lib_room:join_room(State,RoleId).
+leave_room(State,RoleId) -> base_lib_room:leave_room(State,RoleId).
+players_in_room(State) -> base_lib_room:players_in_room(State).
+players_count(State) -> base_lib_room:players_count(State).
 
 
